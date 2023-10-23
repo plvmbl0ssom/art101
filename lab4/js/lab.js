8e3344cc74d6b8bf5e249303094cd3a2a5b2f139
plvmbl0ssom <jazmiiinepearl@gmail.com>
2023-10-23 08:50:41 -0700

    yippee

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,12 +1,13 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of this lab is to gain familiarity with pseudocoding.
+// Virtual Cup Pong
+// Jazmine Noguera <jgnoguer@ucsc.edu> & Maya Anderson <mizenten@ucsc.edu>
+// October 18, 2023
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
+// this is an example function and this comment tells what it does and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
   // return results;
@@ -20,17 +21,22 @@ function main() {
 // let's get this party started
 main();
 
-// initialize the game board
-// set up two players, each with a set of pieces
-// while the game is not over:
-  // display the current state of teh board
-  // ask the current player for their move
-  // if the move is valid:
-    // execute the smove on the board
-    // check for any captures
-    // check for a win condition
-    // switch to the next player
-  // else:
-    // ask the player to make another move
-  // end while
-  // display the winner or declare a draw
+  // Set up two players with 10 cups each, arranged in a pyramid format
+    // Player 1 gets red cups
+    // Player 2 gets blue cups
+    // Each player gets two ping pong balls per round
+  // While the game is in progress:
+    // Display each player’s cups on screen
+    // Ask each player to toss their ping pong balls
+  // If one ball makes it into a cup:
+      // Remove the cup
+    // Ask the next player for their move
+  // If both balls makes it into two cups:
+      // The current player gets two more ping pong balls
+      // Repeat process until all cups are gone
+  // If the ball misses a cup:
+    //  Begin next player’s turn
+  // Else:
+    // If all cups are gone, supply players with one extra cup and ball to begin bonus rounds
+    // End with one winner
+  
