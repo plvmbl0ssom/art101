e46342b1a6465967526fa0e7bebf754b15d3849d
plvmbl0ssom <jazmiiinepearl@gmail.com>
2023-10-25 17:52:24 -0700

    lab 5!

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
+// index.js - The purpose of this lab is to experiment with variables and data types using JavaScript.
 // Jazmine Noguera <jgnoguer@ucsc.edu>
 // October 23, 2023
 
-// Constants
+// Define Variables
+make = "Mercedes";
+model = "C Class";
+color = "Black";
+year = 2018;
+ownIt = true;
 
-// Functions
+// calculate
+age = 2023 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+//Note, that that is WRITELN with an "L" not WRITEIN
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("ownIt: " + ownIt + "<br>");
+document.writeln("Age: " + age + " years<br>");
