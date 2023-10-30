e91b09fb7611f49e7ff61d02814aedd7d7c46a14
plvmbl0ssom <jazmiiinepearl@gmail.com>
2023-10-29 17:00:10 -0700

    lab 6
    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of this lab is experiment with arrays and objects in JavaScript.
+// Author: Jazmine Noguera
+// Date: October 29, 2023
 
-// Constants
+// Define Variables
+myTransport = ["legs", "my housemate's car", "bus", "bike", "2018 Mercedes C Class"];
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+// create an object for my main ride
+myMainRide = {
+  make: "Mercedes",
+  model:"C Class",
+  color: "Black",
+  year: 2018,
+// we can define a function within our object (called a method)
+// that uses the value of year above (using this.year)
+age: function() {
+  return 2023 - this.year
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// output
+document.writeln("My Trip Options: ", myTransport, "</br>");
+// this little trick allows us to write an object to the document
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
