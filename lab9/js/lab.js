a8a3a9d1ef1814e1d731ae7b2d775e80ff233b95
plvmbl0ssom <jazmiiinepearl@gmail.com>
2023-11-12 11:19:31 -0800

    lab 9 js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,22 +5,36 @@
 
 // add button to challenge section
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-$("button-challenge")
 
 // add a click listener to the challenge button
-$("button-challenge").click(function(){
+$("#button-challenge").click(function(){
     // now add (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
 
 // add button to problems section
-$("#problems").append("<button id='button-problems'>Add Highlight</button>");
-$("button-problems")
+$("#problems").append("<button id='button-problems'>Toggle Highlight</button>");
+
+// add a click listener to the problems button
+$("#button-problems").click(function(){
+    // now add (or subtract) the "highlight" class to the section
+    $("#problems").toggleClass("highlight");
+});
 
 // add button to reflection section
-$("#reflection").append("<button id='button-reflection'>Add Highlight</button>");
-$("button-reflection")
+$("#reflection").append("<button id='button-reflection'>Toggle Highlight</button>");
+
+// add a click listener to the reflection button
+$("#button-reflection").click(function(){
+    // now add (or subtract) the "highlight" class to the section
+    $("#reflection").toggleClass("highlight");
+});
 
 // add button to results section
-$("#results").append("<button id='button-results'>Add Highlight</button>");
-$("button-results")
+$("#results").append("<button id='button-results'>Toggle Highlight</button>");
+
+// add a click listener to the results button
+$("#button-results").click(function(){
+    // now add (or subtract) the "highlight" class to the section
+    $("#results").toggleClass("highlight");
+});
