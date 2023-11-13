d967cebd7e4570e4c220ff039d62c47047d1e435
plvmbl0ssom <jazmiiinepearl@gmail.com>
2023-11-12 17:36:26 -0800

    task x lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -12,11 +12,12 @@ $("#button-challenge").click(function(){
     $("#challenge").toggleClass("special");
 });
 
+// TASK X
+
 // Hint 1: If you give all three sections a class (".minor-section"), you can add a button to all three in one jQuery selector and method call.
 // Hint 2: If you use a jQuery selector that selects all three new buttons, you can attach an event listener to all three at the same time 
 $(".minor-section").append("<button id='minor-section button'>Toggle Highlight</button>");
 
-// TASK X
 // Hint 3: Within a jQuery callback the keyword $(this) refers to the thing being selected. The jQuery
 // method .parent() selects the parent of the selected element.
 // So within the callback, we can use $(this).parent() to select the section that the button is in!
