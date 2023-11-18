32fccc6d3d25e7a87e401dff57cf6bc5f034bb8f
plvmbl0ssom <jazmiiinepearl@gmail.com>
2023-11-18 15:13:39 -0800

    lab11 start

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - This simple JavaScript/jQuery script appends new elements to an output div
+// lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified version.
 // Requirements: jQuery must be loaded for this script to work.
 // Author: Jazmine Noguera
-// Date: November 16, 2023
+// Date: November 18, 2023
 
 var name = $("#input").val
