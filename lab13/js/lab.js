094260c068cb4604b1fadf99e25c95c7793c9d15
plvmbl0ssom <jazmiiinepearl@gmail.com>
2023-11-26 21:44:31 -0800

    lab13 (fail)

---
+++ lab13/js/lab.js
@@ -0,0 +1,62 @@
+// lab.js - Using a function to experiment with loops.
+// Requirements: jQuery must be loaded for this script to work.
+// Author: Jazmine Noguera
+// Date: November 26, 2023
+
+// thanks professor modes for the code!
+maxFactors = 4;
+outputEl = document.getElementById("output");
+function getFactorObj() {
+    var factorObj = {};
+    for (var factor=0; factor<maxFactors; factor++) {
+        numId = "num" + factor;
+        textId = "text" + factor;
+        numValue = document.getElementById(numId).value;
+        textValue = document.getElementById(textId).value;
+        console.log(factor + ") num:", numValue, "text:", textValue)
+        if (numValue && textValue) {
+            factorObj[numValue] = textValue;
+        }
+    }
+    return factorObj;
+}
+function outputToPage(str) {
+    newEl = document.createElement("p");
+    newEl.innerHTML = str;
+    outputEl.appendChild(newEl);
+}
+function fizzBuzzBoom(maxNums, factorObj) {
+    for (var num=0; num<maxNums; num++) {
+        debugger;
+        var outputStr = "";
+        for (var factor in factorObj) {
+            if (num % factor == 0) {
+                outputStr += factorObj[factor];
+            }
+        }
+        if (outputStr) {
+            outputStr = " - " + outputStr + "!";
+        }
+        outputToPage(num.toString() + outputStr)
+    }
+}
+function reportError(str) {
+    outputEl.innerHTML = "<div class='error'>" + str + "</div>";
+}
+document.getElementById("submit").addEventListener("click", function() {
+    var max = document.getElementById("max").value;
+    console.log("max:", max)
+    if (! max) {
+        reportError("You must provide a maximum");
+        return;
+    }
+    var factorObj = getFactorObj();
+    console.log("factorObj:", factorObj);
+    if (Object.keys(factorObj).length === 0) {
+        reportError("You must provide at least one factor and text");
+        return;
+    }
+    outputEl.innerHTML = "";
+    fizzBuzzBoom(max, factorObj);
+    outputEl.classList.add("cols");
+})
